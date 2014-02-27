6a5d7ad4dcf10b286a619640d1503bed265df278
Ivan Klimchuk <ivan@klimchuk.com>
2014-02-28 01:55:00 +0300

    Deep night codding, save state

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-node_modules/docpad/bin/docpad clean && node_modules/docpad/bin/docpad run --env $1 --port 12000
+# node_modules/docpad/bin/docpad clean && 
+node_modules/docpad/bin/docpad run --env $1 --port 12000
