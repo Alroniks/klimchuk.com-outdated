097725af57d72415c4d6eb13ad70894da974b67a
Ivan Klimchuk <ik@onliner.by>
2014-02-28 18:08:49 +0300

    Fixed all files

--- run.sh
+++ run.sh
@@ -1,2 +1,1 @@
-# node_modules/docpad/bin/docpad clean && 
-node_modules/docpad/bin/docpad run --env $1 --port 12000
+node_modules/docpad/bin/docpad clean && node_modules/docpad/bin/docpad run --env $1 --port 12000
